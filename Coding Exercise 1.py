3c8cb18a1f18e2ab376df25fe1b895910b0a6f81
tbale101 <85025108+tbale101@users.noreply.github.com>
2021-06-03 21:08:40 +0100

    Add files via upload

---
+++ Coding Exercise 1.py
@@ -0,0 +1,9 @@
+# Write a prigramme that asks the user
+# to enter a weight in kilograms.
+# The program should convert it to pounds.
+# using the formula
+# 1 kg = 2.205 lb
+
+kg = float(input('Enter weight in Kg to Convert into pounds:'))
+pounds = kg * 2.205
+print(kg,'Kilograms =',pounds,'Pounds')
