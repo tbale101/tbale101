628af08fbbbb3d5d8354e380c2324c28bc34bac9
tbale101 <85025108+tbale101@users.noreply.github.com>
2021-06-27 23:05:51 +0100

    Add files via upload

---
+++ My Book 1.py
@@ -0,0 +1,16 @@
+#multiline examples
+print("""This is a Simple
+website example of
+different kind og Fruits""")
+website = "Welcom.com"
+print(website)
+Fruits = ['Strawberry',
+          'Mango',
+          'Kiwi']
+print(Fruits)
+a, b, c = 101, 1001, "Hi"
+
+print (a)
+print (b)
+print (c)
+name = input("Please enter your name: ")
