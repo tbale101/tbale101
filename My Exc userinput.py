ff0cdc0b73af8e5783cadb8ddc8f4e95b6cd6a3e
tbale101 <85025108+tbale101@users.noreply.github.com>
2021-06-29 23:39:25 +0100

    Add files via upload

---
+++ My Exc userinput.py
@@ -0,0 +1,13 @@
+#x = int(input("Enter 1st Num : "))
+#y = int(input("Enter 2nd Num : "))
+#z = x + y
+#print(Results:", z)
+#input()
+print("")
+x = input("Enter any Char: ")
+while len(x) != 1:
+    print("")
+    print("Please enter single char only\n")
+    x = input("Enter any Char: ")
+print("\n",x)
+input()
