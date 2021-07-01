da5003d7fd240e066a895d4cf421514dec078c84
tbale101 <85025108+tbale101@users.noreply.github.com>
2021-07-01 23:00:21 +0100

    Add files via upload

---
+++ Another way dict.py
@@ -0,0 +1,12 @@
+def mass2height(a,b,name):
+    # dict1={'mass':48, 'height':169, 'name':'derrick'};
+    # a=dict1['mass'];
+    # b=dict1['height'];
+    # mass2height= a/b;
+    # print("The mass to height ratio is {0}".format(mass2height))
+
+    dict1={'mass':a, 'height':b, 'name':name};
+    a=dict1['mass'];
+    b=dict1['height'];
+    Rmass2height= a/b;
+    print("The mass to height ratio is {0}".format(Rmass2height))
