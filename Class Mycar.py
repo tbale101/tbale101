08879aac277adaf79f84996e1f8c8db0a11966b6
tbale101 <85025108+tbale101@users.noreply.github.com>
2021-07-01 23:01:31 +0100

    Add files via upload

---
+++ Class Mycar.py
@@ -0,0 +1,8 @@
+class Mycar:
+    def __init__(self,Enginesize, Fuel):
+       self.Enginesize = Enginesize
+       self.Fuel = Fuel
+    
+a1 = Mycar("Volvo",2.0)
+print(a1.Enginesize)
+print(a1.Fuel)
